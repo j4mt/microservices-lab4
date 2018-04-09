8877ecbaab2fdb3faf4fd9f226ffbb57a1be6fa3
John O'Rourke <john.orourke@sqs.com>
2018-04-09 15:36:23 +0100

    fixing dependency

--- lab-4-eureka-web-sentence/src/main/java/com/kennyk65/lab4eurekasentence/controller/SentenceController.java
+++ lab-4-eureka-web-sentence/src/main/java/com/kennyk65/lab4eurekasentence/controller/SentenceController.java
@@ -1,7 +1,11 @@
 package com.kennyk65.lab4eurekasentence.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,7 @@ public class SentenceController {
     }
 
     @Autowired
+    DiscoveryClient client;
     public String getWord(String service) {
         List<ServiceInstance> list = client.getInstances(service);
         if (list != null && list.size() > 0 ) {
